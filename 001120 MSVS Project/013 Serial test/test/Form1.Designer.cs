2a45f3d4f166ebe033533ef52be0748867c7850b
Loy Vanich <laploy@gmail.com>
2018-06-18 05:20:33 +0700

    Add files via upload

---
+++ 001120 MSVS Project/013 Serial test/test/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loySerialPortUc1 = new LoySerialPortUserControl.LoySerialPortUc();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loySerialPortUc1
+            // 
+            this.loySerialPortUc1.Location = new System.Drawing.Point(17, 273);
+            this.loySerialPortUc1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.loySerialPortUc1.Name = "loySerialPortUc1";
+            this.loySerialPortUc1.RxMode = "text";
+            this.loySerialPortUc1.Size = new System.Drawing.Size(219, 30);
+            this.loySerialPortUc1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(17, 10);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(361, 245);
+            this.textBox1.TabIndex = 1;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(266, 273);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(102, 30);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 330);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.loySerialPortUc1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LoySerialPortUserControl.LoySerialPortUc loySerialPortUc1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ 001120 MSVS Project/013 Serial test/test/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace test
+{ 
+    public partial class Form1 : Form
+    {
+        private Timer myTimer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            myTimer.Interval = 500;
+            myTimer.Enabled = false;
+            myTimer.Tick += MyTimer_Tick;
+            loySerialPortUc1.OnDataReceived += LoySerialPortUc1_OnDataReceived;
+        }
+
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+            loySerialPortUc1.Write("A");
+            textBox1.AppendText("A");
+        }
+
+        private void LoySerialPortUc1_OnDataReceived(string recieveString)
+        {
+            textBox1.Invoke(new Action(() =>
+            { textBox1.AppendText(recieveString); }));
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            myTimer.Enabled = true;
+        }
+    }
+}
